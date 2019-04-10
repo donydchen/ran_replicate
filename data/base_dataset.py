741e598b54ba642daca00fedfe4b1437443f8d33
donydchen <donydchen@gmail.com>
2019-04-10 19:51:11 +0800

    add data loader

---
+++ data/base_dataset.py
@@ -0,0 +1,93 @@
+import torch
+import os
+from PIL import Image
+import random
+import numpy as np
+import pickle
+import torchvision.transforms as transforms
+
+
+
+class BaseDataset(torch.utils.data.Dataset):
+    """docstring for BaseDataset"""
+    def __init__(self):
+        super(BaseDataset, self).__init__()
+
+    def name(self):
+        return os.path.basename(self.opt.data_root.strip('/'))
+
+    def initialize(self, opt):
+        self.opt = opt
+        self.imgs_dir = os.path.join(self.opt.data_root, self.opt.imgs_dir)
+        self.is_train = self.opt.mode == "train"
+
+        # load images path 
+        filename = self.opt.train_csv if self.is_train else self.opt.test_csv
+        self.imgs_name_file = os.path.join(self.opt.data_root, filename)
+        self.imgs_path = self.load_image(self.imgs_dir, self.imgs_name_file)
+
+        # load pseudo expression and AU label 
+        pseudo_csv = os.path.join(self.opt.data_root, self.opt.pseudo_csv)
+        self.pseudo_list = self.load_csv(pseudo_csv)
+
+        # load images expression label dicitionary 
+        cls_pkl = os.path.join(self.opt.data_root, self.opt.cls_pkl)
+        self.cls_dict = self.load_dict(cls_pkl)
+
+    def load_image(self, imgs_dir, imgs_name_file):
+        return None
+
+    def load_csv(self, csv_path):
+        return None
+
+    def load_dict(self, pkl_path):
+        saved_dict = {}
+        with open(pkl_path, 'rb') as f:
+            saved_dict = pickle.load(f, encoding='latin1')
+        return saved_dict
+
+    def get_img_by_path(self, img_path):
+        assert os.path.isfile(img_path), "Cannot find image file: %s" % img_path
+        img_type = 'L' if self.opt.img_nc == 1 else 'RGB'
+        return Image.open(img_path).convert(img_type)
+
+    def get_cls_by_path(self, img_path):
+        img_name = os.path.basename(img_path)
+        assert img_name in self.cls_dict, "Cannot find label for %s" % img_name
+        cls_label = self.cls_dict[img_name]
+        return cls_label
+
+    def img_transform(self, img, use_data_augment=False):
+        img2tensor = transforms.Compose([transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+        img = transforms.functional.resize(img, self.opt.load_size)
+        # on-the-fly data augmentation
+        if self.opt.mode == "train" and use_data_augment:
+            # scale and crop 
+            # lucky_num = random.randint(0, 4)
+            lucky_num_crop = random.randint(0, 4)
+            img = transforms.functional.five_crop(img, self.opt.final_size)[lucky_num_crop]
+            # Horizontally flip
+            lucky_num_flip = random.randint(0, 1)
+            if lucky_num_flip:
+                img = transforms.functional.hflip(img)
+        else:
+            img = transforms.functional.five_crop(img, self.opt.final_size)[-1]  # center crop
+
+        return img2tensor(img)
+
+    def __len__(self):
+        return len(self.imgs_path)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ data/ckplus.py
@@ -0,0 +1,56 @@
+from .base_dataset import BaseDataset
+import os
+
+
+class CKPlusDataset(BaseDataset):
+    """docstring for CKPlusDataset"""
+    def __init__(self):
+        super(CKPlusDataset, self).__init__()
+
+    def name(self):
+        return "CKPlusDataset"
+
+    def initialize(self, opt):
+        super(CKPlusDataset, self).initialize(opt)
+        self.EXPRESSION = ['Anger', 'Disgust', 'Fear', 'Happiness', 'Sadness', 'Surprise']
+
+    def load_image(self, imgs_dir, imgs_name_file):
+        imgs = []
+        assert os.path.isfile(imgs_name_file), "File '%s' does not exist." % imgs_name_file
+        with open(imgs_name_file, 'r') as f:
+            lines = f.readlines()
+            imgs = [os.path.join(imgs_dir, line.strip()) for line in lines]
+            imgs = sorted(imgs)
+        return imgs
+
+    def load_csv(self, csv_path):
+        pseudo_list = []
+        assert os.path.isfile(csv_path), "File '%s' does not exist." % csv_path
+        with open(csv_path, 'r') as f:
+            for idx, line in enumerate(f.readlines()):
+                if idx == 0:
+                    continue  # skip the header 
+                cur_list = []
+                items = line.strip().split(',')
+                cur_list.append(self.EXPRESSION.index(items[0]))
+                cur_list.append(list(map(lambda x: float(x), items[1:])))
+                pseudo_list.append(cur_list)
+        return pseudo_list
+
+    def __getitem__(self, index):
+        # load pseudo expression and its AU label
+        pseudo_exp = int(self.pseudo_list[index][0])
+        pseudo_aus = self.pseudo_list[index][1]
+
+        # load image and its expression label 
+        img_path = self.imgs_path[index]
+        img_exp = int(self.get_cls_by_path(img_path))
+        img_tensor = self.img_transform(self.get_img_by_path(img_path), self.opt.use_data_augment)
+
+        data_dict = {'pseudo_aus': pseudo_aus, 'pseudo_exp': pseudo_exp,
+                     'img_exp': img_exp, 'img': img_tensor}
+
+        return data_dict
+
+
+
